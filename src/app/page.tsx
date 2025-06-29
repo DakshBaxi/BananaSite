4c80ba8e2484484554f7f9eaefe6cb971e4bd83f
DakshBaxi <baxidaksh2004@gmail.com>
2025-06-29 11:35:37 +0530

    inngest

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,19 @@
-"use server"
 
-import {  getQueryClient, trpc } from "@/trpc/server";
-import { dehydrate, HydrationBoundary } from "@tanstack/react-query";
-import { Client } from "./client";
-import { Suspense } from "react";
+"use client"
 
-
+import { Button } from "@/components/ui/button";
+import { useTRPC } from "@/trpc/client";
+import { useMutation } from "@tanstack/react-query";
 
 
-export default async function Home() {
-  const queryClient = getQueryClient();
-  void queryClient.prefetchQuery(trpc.createAi.queryOptions({text:"Daksh"}))
+export default  function Page() {
+  const trpc = useTRPC();
+  const invoke = useMutation(trpc.invoke.mutationOptions({}))
   return (
-    <HydrationBoundary state={dehydrate(queryClient)}>
-      <Suspense fallback={<p>Loading...</p>}>
-   <Client/>
-      </Suspense>
-    </HydrationBoundary>
+    <div className="p-4 max-w-7xl mx-auto">
+      <Button onClick={()=>invoke.mutate({text:"Test Job "})}>
+    Invoke Background Job
+      </Button>
+    </div>
   );
 }
